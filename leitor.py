7af6e1f1ea25843ed5c6949ffc6d64a03eea421a
Isabelle-Moreira <106937059+Isabelle-Moreira@users.noreply.github.com>
2023-11-29 10:36:14 -0300

    Add files via upload

--- leitor.py
+++ leitor.py
@@ -8,12 +8,12 @@ caminho_da_pasta ="./arquivos/"
 if not os.path.exists(caminho_da_pasta):
     os.makedirs(caminho_da_pasta)
     
-arquivo = open("ordExt_teste.txt", encoding='latin-1')
+arquivo = open("teste1.txt", encoding='latin-1')
 contador =0
 nome_arquivo = ""
 numero_arquivo=0
 vetor_de_cem =[]
-registros_por_arquivo=100
+registros_por_arquivo=5
 for linha in arquivo:
     #if(contador==0):
         #numero_arquivo+=1
@@ -26,7 +26,7 @@ for linha in arquivo:
             #print(vetor_de_cem)
             numero_arquivo+=1
             nome_arquivo="arquivo"+str(numero_arquivo)+".txt" #nomeando novo arquivo
-            caminho_completo = os.path.join(caminho_da_pasta,nome_arquivo) #caminho completo arquivo + direcionamento para pasta
+            caminho_completo = os.path.join(caminho_da_pasta, nome_arquivo) #caminho completo arquivo + direcionamento para pasta
             #print(caminho_completo)
             novo_arquivo = open(caminho_completo,"a")  #criando novo arquivo já na pasta 
 
@@ -52,3 +52,60 @@ if(len(vetor_de_cem)>0):
         novo_arquivo.write(str(vetor_de_cem[i])+"\n")
 
     novo_arquivo.close()
+
+lista_arquivos = os.listdir(caminho_da_pasta)
+print(lista_arquivos)
+index_inicial =0
+index_final = 5
+incrementa_pros_proximos = 6
+
+
+
+def verifica_se_cabecote_esta_vazio(cabecote):
+    estaVazio = True
+    for elemento in cabecote:
+        if elemento != '':
+            estaVazio = False
+            return estaVazio
+    return estaVazio
+    
+def seleciona_indice_menor_elemento(cabecote):
+    menor_elemento_indice = 0
+    for i in range(1, len(cabecote), 1):
+        if(cabecote[i]==''):
+            continue
+        if(float(cabecote[i]) < float(cabecote[menor_elemento_indice])):
+            menor_elemento_indice = i
+    return menor_elemento_indice
+
+
+while index_inicial < len(lista_arquivos):
+    contador_mesclagem = 0
+    cabecote = [''] * 6
+    lista_arquivos_abertos = []
+
+    for i in range(index_inicial, index_final+1, 1):
+        caminho_completo = os.path.join(caminho_da_pasta, lista_arquivos[i])
+        arquivo = open(caminho_completo)
+
+        lista_arquivos.append(arquivo)
+
+        cabecote[i%incrementa_pros_proximos] = lista_arquivos[i%incrementa_pros_proximos].readline()
+    
+    novo_arquivo_mesclado = open('mesclagem'+str(contador_mesclagem)+'.txt', "a")
+    contador_mesclagem+=1
+
+    #enquanto houver elementos no cabeçote:
+    while not verifica_se_cabecote_esta_vazio(cabecote):
+        index_menor_elemento = seleciona_indice_menor_elemento(cabecote)
+
+        #escreva o menor elemento no novo arquivo
+        novo_arquivo_mesclado.write(cabecote[index_menor_elemento])
+        
+        caminho_completo = os.path.join(caminho_da_pasta, lista_arquivos[index_menor_elemento])
+        arquivo = open(caminho_completo)
+        cabecote[index_menor_elemento] = arquivo.readline()
+
+        index_inicial += incrementa_pros_proximos
+        index_final += incrementa_pros_proximos
+        #mova o cabeçote do arquivo correspondente
