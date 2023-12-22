dfb1aa506fcc0fd3104a09d6d90afd3b349caf0e
limadan <dbatistalima135@gmail.com>
2023-12-22 20:27:57 -0300

    correcao de erro

--- TP2-parte2.py
+++ TP2-parte2.py
@@ -1,7 +1,7 @@
 import os
 import random
 from ordenador import Ordenator
-from metodos_mesclagem import verifica_se_cabecote_esta_vazio, seleciona_indice_menor_elemento 
+from metodos_mesclagem import verifica_se_cabecote_terminou_bloco, verifica_se_cabecote_esta_vazio, seleciona_indice_menor_elemento, verifica_se_tem_arquivo_vazio
 ordenador = Ordenator()
 
 caminho_da_pasta ="./arquivos/"
@@ -18,8 +18,8 @@ vetor_de_elementos = []
 contador_de_registros_em_memoria = 0
 contador_de_registros_em_arquivo = 0
 qtd_maxima_de_registros_em_memoria = 100
-qtd_maxima_de_registros_por_arquivo=10000
 
+print("Criando arquivos")
 for i in range(9):
     numero_arquivo =i+1
     nome_arquivo="arquivo"+str(numero_arquivo)+".txt" #nomeando novo arquivo
@@ -29,21 +29,12 @@ for i in range(9):
 caminho_da_pasta_de_leitura = './arquivos'
 lista_arquivos = os.listdir(caminho_da_pasta_de_leitura)
 
+print("Distribuindo os blocos")
 for linha in arquivo:
-    #if(contador==0):
-        #numero_arquivo+=1
-        #nome_arquivo="arquivo"+str(numero_arquivo)+".txt"
-        #novo_arquivo = open(nome_arquivo,"a")
-    if(contador_de_registros_em_memoria<qtd_maxima_de_registros_por_arquivo):
+    if(contador_de_registros_em_memoria<qtd_maxima_de_registros_em_memoria):
         vetor_de_elementos.append(float(linha)) #vai atribuir os valores da linha do arquivo original no vetor
         contador_de_registros_em_memoria+=1
         if(contador_de_registros_em_memoria==qtd_maxima_de_registros_em_memoria):
-            #print(vetor_de_elementos)
-            if(contador_de_registros_em_arquivo==qtd_maxima_de_registros_por_arquivo):
-                contador_de_registros_em_arquivo = 0
-            
-            if(contador_de_registros_em_arquivo == 0):
-                ordenador.heap_sort(vetor_de_elementos)
         
             #fazer a logica pra acrescentar os elementos do vetor de 100 num arquivo aleatório
             escolhe_aleatorio = random.choice(lista_arquivos)
@@ -52,7 +43,7 @@ for linha in arquivo:
                 ordenador.heap_sort(vetor_de_elementos)
                 arquivo_aleatorio = open(caminho_completo_aleatorio, "a")
                 arquivo_aleatorio.write(str(vetor_de_elementos[i])+"\n")
-                contador_de_registros_em_arquivo += 1
+            arquivo_aleatorio.write("#\n")
 
             arquivo_aleatorio.close()
             contador_de_registros_em_memoria=0
@@ -69,10 +60,87 @@ if(len(vetor_de_elementos)>0):
 
     aleatorio.close()
 
+print("Fazendo as mesclagens")
+
 nome_arquivo="arquivo"+str(10)+".txt" #nomeando novo arquivo
 caminho_completo = os.path.join(caminho_da_pasta, nome_arquivo) #caminho completo arquivo + direcionamento para pasta
 novo_arquivo = open(caminho_completo,"a")  #criando novo arquivo já na pasta
 
+'''
+ETAPAS A SEREM FEITAS
+
+1) Definir o arquivo em que as intercalações serão feitas inicialmente
+
+2) Fazer as intercalações, bloco por bloco, até que um arquivo se esvazie
+    - Fazer a intercalação bloco a bloco
+        - O cabeçote deve intercalar os arquivos até encontrar o caractere de parada ("#")
+        - Ele deve fazer a intercalação até que todo o cabeçote esteja no caractere de parada ("#")
+            - Ele identifica que o bloco foi intercalado
+        - Após, ler a próxima linha de todos os arquivos e repetir o processo
+
+3) Ao esvaziar um arquivo, o arquivo vazio deve ser o novo arquivo em que as intercalações serão feitas
+    - Após fazer a leitura de um bloco, o programa lê a próxima linha
+    - Se a próxima linha do arquivo for VAZIA, significa que o arquivo está vazio
+
+4) Repetir o processo até que todos os arquivos, exceto um, estejam vazios
+
+'''
+
+
+
+nome_do_arquivo_de_intercalacao = 'arquivo10.txt'
+caminho_do_arquivo_de_intercalacao = os.path.join(caminho_da_pasta, nome_do_arquivo_de_intercalacao)
+cabecote_de_leitura = ['', '', '', '', '', '', '', '', '']
+
+lista_de_nomes_de_arquivo = os.listdir(caminho_da_pasta)
+lista_de_nomes_de_arquivo.remove(nome_do_arquivo_de_intercalacao)
+
+lista_de_apontamento_de_arquivos = []
+
+for i in range(len(lista_de_nomes_de_arquivo)):
+    nome = lista_de_nomes_de_arquivo[i]
+        
+    caminho_arquivo = os.path.join(caminho_da_pasta, nome)
+    arquivo = open(caminho_arquivo)
+    cabecote_de_leitura[i] = arquivo.readline()
+
+    lista_de_apontamento_de_arquivos.append(arquivo)
+
+index_arquivo_vazio = verifica_se_tem_arquivo_vazio(cabecote_de_leitura)
+
+while index_arquivo_vazio == None:
+    while not verifica_se_cabecote_terminou_bloco(cabecote_de_leitura):
+        try:
+            index_menor_elemento = seleciona_indice_menor_elemento(cabecote_de_leitura)
+        except:
+            print(cabecote_de_leitura)
+
+        arquivo_de_intercalacao = open(caminho_do_arquivo_de_intercalacao, "a")
+        arquivo_de_intercalacao.write(cabecote_de_leitura[index_menor_elemento])
+
+        arquivo_de_leitura = lista_de_apontamento_de_arquivos[index_menor_elemento]
+        cabecote_de_leitura[index_menor_elemento] = arquivo_de_leitura.readline()
+
+    arquivo_de_intercalacao.write("#\n")
+    print(cabecote_de_leitura)
+
+    for i in range(len(lista_de_apontamento_de_arquivos)):
+        cabecote_de_leitura[i] = lista_de_apontamento_de_arquivos[i].readline()
+
+    print(index_arquivo_vazio)
+    index_arquivo_vazio = verifica_se_tem_arquivo_vazio(cabecote_de_leitura)
+
+arquivo_de_intercalacao = open(caminho_do_arquivo_de_intercalacao, "a")
+
+nome_do_arquivo_de_intercalacao = lista_de_nomes_de_arquivo[index_arquivo_vazio]
+caminho_do_arquivo_de_intercalacao = os.path.join(caminho_da_pasta, nome_do_arquivo_de_intercalacao)
+cabecote_de_leitura = ['', '', '', '', '', '', '', '', '']
+lista_de_nomes_de_arquivo = os.listdir(caminho_da_pasta)
+lista_de_nomes_de_arquivo.remove(nome_do_arquivo_de_intercalacao)
+
+index_arquivo_vazio = verifica_se_tem_arquivo_vazio(cabecote_de_leitura)
+
+print("Terminei!")
 #agora precisa do merge
 
 '''index_inicial =0
--- metodos_mesclagem.py
+++ metodos_mesclagem.py
@@ -5,14 +5,31 @@ def verifica_se_cabecote_esta_vazio(cabecote):
             estaVazio = False
             return estaVazio
     return estaVazio
+
+def verifica_se_tem_arquivo_vazio(cabecote):
+    index_arquivo_vazio = None
+    for index in range(len(cabecote)):
+        if(cabecote[index]==''):
+            index_arquivo_vazio = index
+            return index_arquivo_vazio
+    return index_arquivo_vazio
+
+def verifica_se_cabecote_terminou_bloco(cabecote):
+    terminouLeitura = True
+    for elemento in cabecote:
+        if elemento != '#\n':
+            terminouLeitura = False
+            return terminouLeitura
+    return terminouLeitura
     
 def seleciona_indice_menor_elemento(cabecote):
     menor_elemento_indice = 0
-    while cabecote[menor_elemento_indice] == '':
+    while cabecote[menor_elemento_indice] == '' or cabecote[menor_elemento_indice] == '\n' or cabecote[menor_elemento_indice] == '#\n':
         menor_elemento_indice+=1
     for i in range(1, len(cabecote), 1):
-        if(cabecote[i]==''):
+        if(cabecote[i]=='' or cabecote[i]=='#\n'):
             continue
         if(float(cabecote[i]) < float(cabecote[menor_elemento_indice])):
             menor_elemento_indice = i
     return menor_elemento_indice
+
